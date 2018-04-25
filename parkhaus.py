b89f3e23cf0907f7803c76d0d775db4a57c664ef
Peter20018 <171witb10@o365.htl-leoben.at>
2018-04-25 09:10:20 +0200

    Linienbus Update

--- parkhaus.py
+++ parkhaus.py
@@ -1,15 +1,18 @@
 # parkhaus.py 
 # Angabe für das Beispiel: siehe Moodle
 
+a = 0;
+
 print("Linienbus Simulator 2018!")
 
 haltestellen = input("Wie viele Haltestellen gibt es? ")
 
 haltestelle = int(haltestellen)
 
-for i in range(0,haltestelle):
+for i in range(1,haltestelle+1):
 
-    personen = input("Sie sind an der Haltestelle " ,i, ". Wie viele Personen steigen ein?")
+    print("Sie sind an der Haltestelle " , i , ". Wie viele Personen steigen ein?")
+    personen = input()
     person = int(personen)
-
-    i = i + person
+    a = a + person
+print("Die Anzahl der eingestiegenen Personen beträgt " , a)
