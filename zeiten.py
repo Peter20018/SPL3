2d098abfa179055eed1a53a97a98a2f4f3b828af
Peter20018 <171witb10@o365.htl-leoben.at>
2018-04-25 10:42:33 +0200

    Zeiten.py erstellt

--- zeiten.py
+++ zeiten.py
@@ -1,1 +1,15 @@
 # zeiten.py 
+
+beginnZeit = input("Beginnzeit: ")
+endeZeit = input("Endzeit: ")
+
+beginn = beginnZeit.split(":")
+print(beginn)
+
+h = int(beginn[0])
+m = int(beginn[1])
+s = int(beginn[2])
+
+print("Stunden: " , h)
+print("Minuten: ", m)
+print("Sekunden: ", s)
